bb2511fbf93d9f56ee45f3618a9dc874bd8dddb5
Piotr Grabowski <ziajowaty@gmail.com>
2018-01-09 19:02:35 +0100

    A wiec to tak dziala git

---
+++ zad6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fileName = "tekst.txt";
+
+            FileStream file = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            byte[] buffer = new byte[file.Length];
+            WaitHandle handle = new AutoResetEvent(false);
+
+            file.BeginRead(buffer, 0, (int)file.Length, AsyncCallback, new object[] { file, buffer, handle });
+
+            handle.WaitOne();
+
+            Console.WriteLine("Finished executing main thread. Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        static void AsyncCallback(IAsyncResult state)
+        {
+            var tmp = (object[])state.AsyncState;
+            
+            FileStream file = (FileStream)tmp[0];
+            byte[] buffer = (byte[])tmp[1];
+            AutoResetEvent handle = (AutoResetEvent)tmp[2];
+
+            Console.WriteLine(ASCIIEncoding.ASCII.GetString(buffer));
+            file.EndRead(state);
+            handle.Set();
+        }
+    }
+}
---
+++ zad7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace zad7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fileName = "tekst.txt";
+
+            FileStream file = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            byte[] buffer = new byte[file.Length];
+            WaitHandle handle = new AutoResetEvent(false);
+
+            IAsyncResult iar = file.BeginRead(buffer, 0, (int)file.Length, null, null);
+
+            //w tym miejscu watek sie zatrzymuje, i czeka, az asynchroniczny watek wywolany w file.BeginRead skonczy prace.
+            //dzieki takiemu rozwiazaniu kod wyglada jakby byl jednowatkowy - latwo sie go programuje, a zarazem posiada szybkosc wielowatkowosci
+            file.EndRead(iar);
+
+            Console.WriteLine(Encoding.ASCII.GetString(buffer));
+
+            Console.WriteLine("Finished executing main thread. Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
