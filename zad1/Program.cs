072c00a01b1c0e3724111c36f87623ac930258c5
Piotr Grabowski <ziajowaty@gmail.com>
2018-01-09 16:02:50 +0100

    test

---
+++ zad1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(ThreadProc, new object[] { 1200 });
+            ThreadPool.QueueUserWorkItem(ThreadProc, new object[] { 1000 });
+
+            Console.WriteLine("Main.");
+            Console.ReadKey();
+        }
+
+        // This thread procedure performs the task.
+        static void ThreadProc(Object stateInfo)
+        {
+            //cast data from stateInfo to integer
+            var data = ((object[])stateInfo)[0];
+            int integer = (int)data;
+            Thread.Sleep(integer);
+            //wypisujemy w konsoli ile czekal dany proces
+            Console.WriteLine("{0} waited: {1}", Thread.CurrentThread.ManagedThreadId, integer);
+        }
+    }
+}
